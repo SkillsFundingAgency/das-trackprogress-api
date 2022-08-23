0c698068944c6b5d29afdbc0820882b43bf7772a
Paul Graham <paul.graham@coprime.co.uk>
2022-08-23 09:25:48 +0100

    updated SQL structure

--- src/SFA.DAS.TrackProgress.Database/Tables/ApprenticeProgress.sql
+++ src/SFA.DAS.TrackProgress.Database/Tables/ApprenticeProgress.sql
@@ -6,16 +6,22 @@
     [StartDate] DATETIME2 NOT NULL,
     [ApprenticeshipId] bigint NOT NULL,
     [ApprenticeshipContinuationId] bigint NULL,
-    [ProgressDataVersion] bigint NOT NULL,
+    [ProgressDataVersion] SMALLINT NOT NULL,
     [ProgressData] nvarchar(max) NOT NULL,
 	[CreatedOn] datetime2 NOT NULL DEFAULT current_timestamp, 
     CONSTRAINT [PK_Progress] PRIMARY KEY ([Id])
 );
 GO
 
-CREATE NONCLUSTERED INDEX [IX_Progress_UKPRN_ULN_StartDate] ON [Progress]
+CREATE NONCLUSTERED INDEX [IX_Progress_ProviderId_ULN_StartDate] ON [Progress]
 (
 	[ProviderId] ASC,
 	[Uln] ASC,
 	[StartDate] ASC
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Progress_ApprenticeshipId] ON [Progress]
+(
+	[ApprenticeshipId] ASC
 )
