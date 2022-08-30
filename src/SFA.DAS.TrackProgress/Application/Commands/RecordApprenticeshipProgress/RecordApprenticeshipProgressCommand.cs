170fe3c3cd016b9e9485a0e582ec62f220fd02f4
Ben Arnold <ben@arnoldfamily.me.uk>
2022-08-30 10:44:54 +0100

    🔨 Put command handler with command

--- src/SFA.DAS.TrackProgress/Application/Commands/RecordApprenticeshipProgress/RecordApprenticeshipProgressCommand.cs
+++ src/SFA.DAS.TrackProgress/Application/Commands/RecordApprenticeshipProgress/RecordApprenticeshipProgressCommand.cs
@@ -1,16 +1,36 @@
 ﻿using MediatR;
+using SFA.DAS.TrackProgress.Database;
 using SFA.DAS.TrackProgress.DTOs;
+using SFA.DAS.TrackProgress.Models;
 
 namespace SFA.DAS.TrackProgress.Application.Commands.RecordApprenticeshipProgress;
 
-public class RecordApprenticeshipProgressCommand : IRequest
+public record RecordApprenticeshipProgressCommand(long ApprenticeshipId, ProgressDto Progress) : IRequest;
+
+public class RecordApprenticeshipProgressCommandHandler : IRequestHandler<RecordApprenticeshipProgressCommand>
 {
-    public RecordApprenticeshipProgressCommand(long apprenticeshipId, ProgressDto progress)
+    private readonly TrackProgressContext context;
+
+    public RecordApprenticeshipProgressCommandHandler(TrackProgressContext context)
+        => this.context = context;
+
+    public async Task<Unit> Handle(
+        RecordApprenticeshipProgressCommand request, CancellationToken cancellationToken)
     {
-        ApprenticeshipId = apprenticeshipId;
-        Progress = progress;
+        context.Progress.Add(
+            new Progress(
+                request.Progress.ProviderApprenticeshipIdentifier,
+                new ApprovalId(
+                    request.ApprenticeshipId,
+                    request.Progress.ApprenticeshipContinuationId),
+                new KsbTaxonomy(
+                    ToDomainTaxonomy(request.Progress.Ksbs))));
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
     }
 
-    public long ApprenticeshipId { get; set; }
-    public ProgressDto Progress { get; set; }
+    private static KsbTaxonomyItem[] ToDomainTaxonomy(ProgressItem[] dto)
+        => dto.Select(x => new KsbTaxonomyItem(x.Id, x.Value)).ToArray();
 }
