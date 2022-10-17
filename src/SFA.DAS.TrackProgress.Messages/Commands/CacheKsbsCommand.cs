48b9e9c586998fcf0307ddeb9a21b1abd50dc02f
Paul Graham <paul.graham@coprime.co.uk>
2022-10-17 16:47:38 +0100

    Change CourseId to Standard

--- src/SFA.DAS.TrackProgress.Messages/Commands/CacheKsbsCommand.cs
+++ src/SFA.DAS.TrackProgress.Messages/Commands/CacheKsbsCommand.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.TrackProgress.Messages.Commands
 {
     public class CacheKsbsCommand
     {
-        public long CourseId { get; set; }
+        public string Standard{ get; set; }
         public Guid[] KsbIds { get; set; }
     }
 }
