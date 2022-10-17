a759eb4485c6283a010dbae14c0e6fda382705f5
Paul Graham <paul.graham@coprime.co.uk>
2022-10-17 14:53:00 +0100

    Added CacheKsbCommand

---
+++ src/SFA.DAS.TrackProgress.Messages/Commands/CacheKsbsCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SFA.DAS.TrackProgress.Messages.Commands
+{
+    public class CacheKsbsCommand
+    {
+        public long CourseId { get; set; }
+        public Guid[] KsbIds { get; set; }
+    }
+}
